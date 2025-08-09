b3d24b3b6ab587847e5eeb36fca11cea5ebadab8
wolfguevara <marco.wolf.guevara@gmail.com>
2025-08-09 11:56:44 -0400

    add files

---
+++ service-worker.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'budget-pwa-v1';
+const OFFLINE_ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-512.png',
+  'https://cdn.tailwindcss.com',
+  'https://unpkg.com/react@18/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
+  'https://unpkg.com/@babel/standalone/babel.min.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(OFFLINE_ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k === CACHE_NAME ? null : caches.delete(k))))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  event.respondWith(
+    caches.match(req).then((hit) => {
+      if (hit) return hit;
+      return fetch(req).then((res) => {
+        const resClone = res.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(req, resClone)).catch(()=>{});
+        return res;
+      }).catch(() => {
+        if (req.mode === 'navigate') return caches.match('./index.html');
+        return Promise.reject('offline');
+      });
+    })
+  );
+});
